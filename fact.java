459d2d3317d6bd7e896338da58208d1befd2d47c
DeepakYadav45 <91742463+DeepakYadav45@users.noreply.github.com>
2022-01-18 10:52:56 +0530

    code for Factorial

---
+++ fact.java
@@ -0,0 +1,14 @@
+public class fact {
+    public static void main(String[] args) {
+       int x=5;
+       int fact =1;
+       for(int i=0 ;i<5 ;i++)
+       {
+        fact=fact*x;
+        x--;
+       }
+   System.out.println(fact);
+
+   } 
+    
+}
